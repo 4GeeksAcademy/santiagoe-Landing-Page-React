8f3855b7b63f36f5ecc9f24dfed34b480f8ca04a
santiagoe16 <santiagoespinal333@gmail.com>
2024-08-24 05:46:10 +0000

    Lading Page React With Bootstrap

---
+++ src/js/component/card.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes  from "prop-types";
+
+const Card = (prop) =>{
+    return(
+        <div className="card text-center justify-content-between" style={{width: "305px", height: "480px"}}>
+            <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="img"/>
+            <div className="card-body justify-content-center">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">{prop.text}</p>
+            </div>
+            <div className="card-footer">
+                    <a href="#" className="btn btn-primary">Find Out More!</a>
+            </div>
+        </div>
+    );
+}
+Card.ProtoTypes = {
+    text: PropTypes.string
+}
+
+export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import Navbar from "./navbar";
+import Jumbotron from "./jumbotron";
+import Card from "./card";
+import Footer from "./footer";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<Navbar></Navbar>
+			<div className="container">
+				<Jumbotron></Jumbotron>
+				<div className="row d-flex mb-3">
+					<div className="col d-flex justify-content-center"><Card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam, consequuntur amet, nemo dolorum nam esse."></Card></div>
+					<div className="col d-flex justify-content-center"><Card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam, consequuntur amet, nemo dolorum nam esse animi fuga repudiandae id facilis dolor."></Card></div>
+					<div className="col d-flex justify-content-center"><Card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam, consequuntur amet, nemo dolorum nam esse."></Card></div>
+					<div className="col d-flex justify-content-center"><Card text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo aliquam, consequuntur amet, nemo dolorum nam esse animi fuga repudiandae id facilis dolor."></Card></div>
+				</div>
+			</div>
+			<Footer></Footer>
+		</>
 	);
 };
 
